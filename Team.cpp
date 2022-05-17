a2b8ea8d7bc303b7de22d3e0d6e749aed080feb0
pploski <01168908@pw.edu.pl>
2022-05-17 16:21:30 +0200

    basic methods and attributes implemented

--- Team.cpp
+++ Team.cpp
@@ -1,3 +1,25 @@
 #include "Team.h"
 #include <iostream>
 
+void Team::add_member(Player const& p1)
+{
+    team_members.push_back(p1);
+}
+
+void Team::set_elo(unsigned int new_elo)
+{
+    elo_p = new_elo;
+}
+
+void Team::print()
+{
+    std::cout << "Team name: " << name << std::endl;
+    std::cout << "Team id: " << id << std::endl;
+    std::cout << "Team elo: " << elo_p << std::endl;
+    std::cout << "Team members: " << name << std::endl;
+    for (int i = 0; i < team_members.size(); i++)
+    {
+        team_members[i].print();
+    }
+
+}
--- Team.h
+++ Team.h
@@ -1,11 +1,26 @@
 #pragma once
-#include "League.h"
+#include "Player.h"
+#include <string>
+#include <vector>
 
 class Team
 {
 private:
     unsigned int id;
+    std::string name;
+    std::vector <Player> team_members;
+    int elo_p;
 public:
-    Team(unsigned int t_id) : id(t_id){};
-    ~Team();
+    Team(unsigned int t_id, std::string n, std::vector <Player> tm, int elo)
+    : id(t_id), name(n), team_members(tm), elo_p(elo){};
+
+    Team(unsigned int t_id, std::string n, std::vector <Player> tm)
+    : id(t_id), name(n), team_members(tm), elo_p(1000){};
+
+    unsigned int get_id() const {return id;};
+    std::string get_name() const {return name;};
+    int get_elo() const {return elo_p;};
+    void add_member(Player const& p1);
+    void set_elo(unsigned int new_elo);
+    void print();
 };
