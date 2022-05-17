947721a9aace731be9522592f30be22535c6fdbb
pploski <01168908@pw.edu.pl>
2022-05-17 16:20:04 +0200

    Player print and set_elo implemented

--- Player.cpp
+++ Player.cpp
@@ -1,3 +1,17 @@
 #include "Player.h"
 #include <iostream>
 
+
+void Player::set_elo(unsigned int new_elo)
+{
+    elo_p = new_elo;
+}
+
+
+void Player::print()
+{
+    std::cout << "Player name: " << name << std::endl;
+    std::cout << "Player id: " << id << std::endl;
+    std::cout << "Player elo: " << elo_p << std::endl;
+}
+
