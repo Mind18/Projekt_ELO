ddd5c85127f44b9ef9d72f3608ab9c7de14f12eb
pploski <01168908@pw.edu.pl>
2022-05-18 20:25:54 +0200

    added operators and getters

--- Match.h
+++ Match.h
@@ -16,4 +16,12 @@ public:
     Match(unsigned int m_id, T* p1, T* p2, double win)
     : id(m_id), participant1(p1), participant2(p2), winner(win) {};
 
+    unsigned int get_id(){return id;};
+    T* get_team_1(){return &participant1;};
+    T* get_team_2(){return &participant2;};
+    double get_winner(){return winner;};
+
+    bool operator==(Match m1) {return id==m1.get_id();};
+    bool operator!=(Match m1) {return not operator==(m1);};
+
 };
