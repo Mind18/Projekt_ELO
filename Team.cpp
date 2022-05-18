2b292505fdfdc6ab6d763c02163e3b3c449b0752
pploski <01168908@pw.edu.pl>
2022-05-18 19:58:55 +0200

    added remove_member(), added n_members atrribute with getter

--- Team.cpp
+++ Team.cpp
@@ -4,15 +4,29 @@
 
 using namespace std;
 
-void Team::add_member(Player const& p1)
-{
-    team_members.push_back(p1);
-}
-
 void Team::set_elo(unsigned int new_elo)
 {
     elo_p = new_elo;
 }
+
+void Team::add_member(Player const& p1)
+{
+    team_members.push_back(p1);
+    n_members++;
+}
+
+void Team::remove_member(unsigned int id)
+{
+    for(int i = 0; i < team_members.size(); i++)
+    {
+        if (team_members[i].get_id() == id)
+        {
+            team_members.erase(team_members.begin() + i);
+            n_members--;
+        }
+    }
+}
+
 
 void Team::print()
 {
--- Team.h
+++ Team.h
@@ -11,21 +11,29 @@ private:
     unsigned int id;
     string name;
     vector <Player> team_members;
+    unsigned int n_members;
     int elo_p;
 public:
     Team(unsigned int t_id, string n, vector <Player> tm, int elo)
-    : id(t_id), name(n), team_members(tm), elo_p(elo){};
+    : id(t_id), name(n), team_members(tm), n_members(tm.size()), elo_p(elo){};
 
     Team(unsigned int t_id, string n, vector <Player> tm)
-    : id(t_id), name(n), team_members(tm), elo_p(1000){};
+    : id(t_id), name(n), team_members(tm), n_members(tm.size()), elo_p(1000){};
 
     unsigned int get_id() const {return id;};
     string get_name() const {return name;};
-    int get_elo() const {return elo_p;};
     vector<Player> get_members() {return team_members;};
+    unsigned int get_n_members() {return n_members;};
+
     void add_member(Player const& p1);
+    void remove_member(unsigned int id);
+
+    int get_elo() const {return elo_p;};
     void set_elo(unsigned int new_elo);
+    void recalculate_elo();
+
     void print();
+
     bool operator==(Team const& other);
     bool operator!=(Team const& other)
     {
