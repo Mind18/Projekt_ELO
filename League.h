73f13cacb218d7d3598a833c5ed1623633f3246c
pploski <01168908@pw.edu.pl>
2022-05-19 19:20:13 +0200

    added map type of standings

--- League.h
+++ League.h
@@ -3,6 +3,8 @@
 
 #include<iostream>
 #include<vector>
+#include<map>
+#include<fstream>
 
 using namespace std;
 
@@ -10,16 +12,16 @@ template <typename T> class League
 {
 private:
     unsigned int id;
-    vector<T> Participants;
-    vector<Match <T>> match_schedule;
-    vector<T> standings; /* do zmiany pewnie */
+    std::vector<T> Participants;
+    std::vector<Match <T>> match_schedule;
+    std::map<T, double> standings;
     double pts_win;
     double pts_draw;
     double pts_lose;
     bool is_draw_allowed;
 public:
-    League(unsigned int l_id, vector<T> members, vector<Match<T>> matches, vector<T> table,
-    double win, double draw, double lose,  bool draw_allow)
+    League(unsigned int l_id, vector<T> members, vector<Match<T>> matches,
+    std::map<T, double> table, double win, double draw, double lose,  bool draw_allow)
     : id(l_id), Participants(members), match_schedule(matches), standings(table), pts_win(win),
     pts_lose(lose), pts_draw(draw_allow ? draw : 0), is_draw_allowed(draw_allow){};
 
@@ -29,5 +31,7 @@ public:
     double get_pts_win(){return pts_win;}
     double get_pts_draw(){return pts_draw;}
     double get_pts_lose(){return pts_lose;}
+
+    void set_schedule(vector<Match <T>> new_match_schedule){match_schedule = new_match_schedule;};
 
 };
