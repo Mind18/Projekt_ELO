8009579e5e4be9f3462eab7aff3993eb99ef7175
pploski <01168908@pw.edu.pl>
2022-05-21 18:05:16 +0200

    commented because of refference error

--- League.cpp
+++ League.cpp
@@ -21,19 +21,31 @@ using namespace std;
 //     throw wrong_id;
 // };
 
-template <typename T> double League<T>::simulate_match(unsigned int part_id, unsigned int part2_id, unsigned int last_match_id)
-{
-    Match <T> m1 (last_match_id, part_id, part2_id);
-    m1.determine_winner(part_id, part2_id);
-    return m1.get_participant_result(part_id);
-}
+// template <typename T> double League<T>::simulate_match(unsigned int part_id, unsigned int part2_id, unsigned int last_match_id)
+// {
+//     Match <T> m1 (last_match_id + 1, *this,get_participant_by_id(part_id),
+//     *this,get_participant_by_id(part2_id));
+//     return m1.get_participant_result(m1.determine_winner(/*code here*/));
+// }
 
-template <typename T> void League<T>::print_standings()
-{
-    typename std::map<T, double>::iterator it = standings.begin();
-    while (it != standings.end())
-    {
-        std::cout << "Participant: " << it->first.get_name() << " points: " << it->second << std::endl;
-        it++;
-    }
-}
+// template <typename T> void League<T>::create_schedule(bool rematch)
+// {
+//     for(int i=Participants.size()-1; i>=0; i--)
+//     {
+//         for(int j=i-1; j>=0; j--)
+//         {
+//             continue;
+//         }
+//         std::cout << "ok\n";
+//     }
+// }
+
+// template <typename T> void League<T>::print_standings()
+// {
+//     typename std::map<T, double>::iterator it = standings.begin();
+//     while (it != standings.end())
+//     {
+//         std::cout << "Participant: " << it->first.get_name() << " points: " << it->second << std::endl;
+//         it++;
+//     }
+// }
