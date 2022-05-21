5ab19c03b1c6b76d38362cf2e57c4370daac58e8
pploski <01168908@pw.edu.pl>
2022-05-21 17:21:48 +0200

    added exepction

--- Match.h
+++ Match.h
@@ -3,7 +3,7 @@
 using namespace std;
 
 enum match_result {Participant1, Participant2, Draw, TBD};
-enum my_exceptions {illegal_result, invalid_participant};
+enum my_exceptions {illegal_result, invalid_participant, wrong_id};
 
 template <typename T> class Match
 {
