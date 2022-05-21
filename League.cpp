7d5b910416c68590e8a845a482731ee4bf7a19f4
pploski <01168908@pw.edu.pl>
2022-05-21 17:20:56 +0200

    implemented simulate_match() in League class

--- League.cpp
+++ League.cpp
@@ -2,11 +2,31 @@
 
 #include <iostream>
 #include <map>
+#include <string>
 #include <vector>
 #include <fstream>
 
 using namespace std;
 
+
+// template <typename T> T League<T>::get_participant_by_id(unsigned int part_id)
+// {
+//     for(int i=0; i < Participants.size(); i++)
+//     {
+//         if(Participants[i].get_id() == part_id)
+//         {
+//             return Participants[i];
+//         }
+//     }
+//     throw wrong_id;
+// };
+
+template <typename T> double League<T>::simulate_match(unsigned int part_id, unsigned int part2_id, unsigned int last_match_id)
+{
+    Match <T> m1 (last_match_id, part_id, part2_id);
+    m1.determine_winner(part_id, part2_id);
+    return m1.get_participant_result(part_id);
+}
 
 template <typename T> void League<T>::print_standings()
 {
