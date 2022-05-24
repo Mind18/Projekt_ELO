7638e7700eaf2e6f702f17866d9cb2fc4c6c00f8
pploski <01168908@pw.edu.pl>
2022-05-24 11:36:25 +0200

    tried to move methods to .cpp

--- League.cpp
+++ League.cpp
@@ -21,24 +21,23 @@ using namespace std;
 //     throw wrong_id;
 // };
 
-// template <typename T> double League<T>::simulate_match(unsigned int part_id, unsigned int part2_id, unsigned int last_match_id)
-// {
-//     Match <T> m1 (last_match_id + 1, *this,get_participant_by_id(part_id),
-//     *this,get_participant_by_id(part2_id));
-//     return m1.get_participant_result(m1.determine_winner(/*code here*/));
-// }
+template <typename T> double League<T>::simulate_match(unsigned int part_id, unsigned int part2_id, unsigned int last_match_id)
+{
+    Match <T> m1 (last_match_id + 1, *this,get_participant_by_id(part_id),
+    *this,get_participant_by_id(part2_id));
+    return m1.get_participant_result(m1.determine_winner(/*code here*/));
+}
 
-// template <typename T> void League<T>::create_schedule(bool rematch)
-// {
-//     for(int i=Participants.size()-1; i>=0; i--)
-//     {
-//         for(int j=i-1; j>=0; j--)
-//         {
-//             continue;
-//         }
-//         std::cout << "ok\n";
-//     }
-// }
+template <typename T> void League<T>::create_schedule(bool rematch)
+{
+    for(int i=Participants.size()-1; i>=0; i--)
+    {
+        for(int j=i-1; j>=0; j--)
+        {
+            continue;
+        }
+    }
+}
 
 // template <typename T> void League<T>::print_standings()
 // {
--- League.h
+++ League.h
@@ -27,6 +27,7 @@ public:
 
     unsigned int get_id(){return id;};
     vector <T> get_participants(){return Participants;};
+
     T get_participant_by_id(unsigned int part_id)
     {
     for(int i=0; i < Participants.size(); i++)
@@ -37,7 +38,8 @@ public:
         }
     }
     throw wrong_id;
-    };
+};
+
     vector<Match <T>> get_match_schedule(){return match_schedule;};
     double get_pts_win(){return pts_win;};
     double get_pts_draw(){return pts_draw;};
@@ -45,25 +47,10 @@ public:
 
     void set_schedule(vector<Match <T>> new_match_schedule){match_schedule = new_match_schedule;};
 
-    double simulate_match(unsigned int part_id, unsigned int part2_id, unsigned int last_match_id)
-    {
-        unsigned int m_id = last_match_id + 1;
-        Match <T> m1 (m_id, get_participant_by_id(part_id),
-        get_participant_by_id(part2_id));
-        return m1.get_participant_result(m1.determine_winner(/*code here*/));
-    }
+    double simulate_match(unsigned int part_id, unsigned int part2_id, unsigned int last_match_id);
+
     void simulate_league();
-    void create_schedule(bool rematch)
-    {
-        for(int i=Participants.size()-1; i>=0; i--)
-        {
-            for(int j=i-1; j>=0; j--)
-            {
-                continue;
-            }
-            std::cout << "ok\n";
-        }
-    }
+    void create_schedule(bool rematch);
 
     void read_schedule();
 
