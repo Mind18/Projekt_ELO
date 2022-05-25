277d23e7e784d33a6782238117686031ee8741a4
pploski <01168908@pw.edu.pl>
2022-05-25 22:51:37 +0200

    error fixed

--- League.cpp
+++ League.cpp
@@ -27,8 +27,8 @@ template <typename T> T League<T>::get_participant_by_id(unsigned int part_id)
 
 template <typename T> double League<T>::simulate_match(unsigned int part_id, unsigned int part2_id, unsigned int last_match_id)
 {
-    Match <T> m1 (last_match_id + 1, *this.get_participant_by_id(part_id),
-    *this.get_participant_by_id(part2_id));
+    Match <T> m1 (last_match_id + 1, get_participant_by_id(part_id),
+    get_participant_by_id(part2_id));
     return m1.get_participant_result(m1.determine_winner(/*code here*/));
 }
 
