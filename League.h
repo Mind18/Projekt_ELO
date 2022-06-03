390d2a65283b24dd0435e80410fa0630bada9b1d
pploski <01168908@pw.edu.pl>
2022-06-03 20:04:51 +0200

    added setters for menu purpose

--- League.h
+++ League.h
@@ -36,6 +36,11 @@ public:
     double get_pts_lose(){return pts_lose;};
     bool get_draw_allowed(){return is_draw_allowed;};
 
+    void set_pts_win(int win_points){pts_win = win_points;};
+    void set_pts_draw(int draw_points){pts_draw = draw_points;};
+    void set_pts_lose(int lose_points){pts_lose = lose_points;};
+    void set_draw_allowed(bool allow_draw){is_draw_allowed = allow_draw;};
+
     void set_schedule(vector<Match <T>> new_match_schedule){match_schedule = new_match_schedule;};
 
     double simulate_match(unsigned int part_id, unsigned int part2_id, unsigned int last_match_id);
